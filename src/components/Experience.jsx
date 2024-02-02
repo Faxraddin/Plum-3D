5b12c83adc33f08c6c95f33d034cb1405cfcd6b3
Faxraddin <reydivonay@gmail.com>
2024-02-02 22:25:48 +0400

    added line

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,44 @@
-import { Float, OrbitControls } from "@react-three/drei";
+import { Float, Line, OrbitControls } from "@react-three/drei";
 import Background from "./Background";
+import * as THREE from 'three';
 
 import Airplane from "./Airplane";
 import Cloud from "./Cloud";
+import { useMemo } from "react";
 
 export const Experience = () => {
+
+  const curve = useMemo(()=>{
+    return new THREE.CatmullRomCurve3([
+      new THREE.Vector3(0,0,0),
+      new THREE.Vector3(0,0,-10),
+      new THREE.Vector3(-2,0,-20),
+      new THREE.Vector3(-3,0,-30),
+      new THREE.Vector3(0,0,-40),
+      new THREE.Vector3(5,0,-50),
+      new THREE.Vector3(7,0,-60),
+      new THREE.Vector3(5,0,-70),
+      new THREE.Vector3(0,0,-80),
+      new THREE.Vector3(0,0,-90),
+      new THREE.Vector3(0,0,-100),
+    ],
+    false,
+    "catmullrom",
+    0.5)
+  });
+
+  const linePoints = useMemo(()=>{
+    return curve.getPoints(2000)
+  },[curve]);
+
+  const shape = useMemo(() => {
+    const shape = new THREE.Shape();
+    shape.moveTo(0, -0.2);
+    shape.lineTo(0, 0.2);
+
+    return shape;
+  }, [curve]);
+
   return (
     <>
       <OrbitControls />
@@ -18,6 +52,22 @@ export const Experience = () => {
         />
       </Float>
 
+      <group position-y={-2}>
+      
+        <mesh>
+          <extrudeGeometry
+            args={[
+              shape,
+              {
+                steps:2000,
+                extrudePath:curve,
+                bevelEnabled:false
+              }
+            ]}
+          />
+          <meshStandardMaterial color={'white'} lineWidth={16} transparent opacity={0.7}/>
+        </mesh>
+      </group>
 
       <Cloud opacity={0.5} scale={[0.3, 0.3, 0.3]} position={[-2, 1, -3]} />
       <Cloud opacity={0.5} scale={[0.2, 0.3, 0.4]} position={[1.5, -0.5, -2]} />
