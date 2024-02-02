c9eda393c662f20c08b9be01b0592d6e84f34c45
Faxraddin <reydivonay@gmail.com>
2024-02-02 21:40:25 +0400

    added animation to helix

--- src/components/Airplane.jsx
+++ src/components/Airplane.jsx
@@ -1,12 +1,19 @@
 import React, { useRef } from 'react';
 import { useGLTF } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
 
 const Airplane = (props) => {
-    const { nodes, materials } = useGLTF('/models/airplane/model.glb')
+    const { nodes, materials } = useGLTF('/models/airplane/model.glb');
+    const helix = useRef();
+
+    useFrame((state,delta)=>{
+        helix.current.rotation.x += delta * 6;
+    })
+
     return (
         <group {...props} dispose={null}>
             <mesh geometry={nodes.PUSHILIN_Plane_Circle000.geometry} material={materials.plane} />
-            <mesh geometry={nodes.PUSHILIN_Plane_Helix.geometry} material={materials.plane} position={[1.09, 0.23, 0]} />
+            <mesh ref={helix} geometry={nodes.PUSHILIN_Plane_Helix.geometry} material={materials.plane} position={[1.09, 0.23, 0]} />
         </group>
     )
 };
