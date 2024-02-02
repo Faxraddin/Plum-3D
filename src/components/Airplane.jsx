343a2adaaaec994278e891c5b1f092c6da51d421
Faxraddin <reydivonay@gmail.com>
2024-02-02 22:08:48 +0400

    added clouds,plane and scale

--- src/components/Airplane.jsx
+++ src/components/Airplane.jsx
@@ -1,22 +1,32 @@
-import React, { useRef } from 'react';
-import { useGLTF } from '@react-three/drei';
-import { useFrame } from '@react-three/fiber';
+import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import React, { useRef } from "react";
 
-const Airplane = (props) => {
-    const { nodes, materials } = useGLTF('/models/airplane/model.glb');
-    const helix = useRef();
+const HELIX_SPEED = 6;
 
-    useFrame((state,delta)=>{
-        helix.current.rotation.x += delta * 6;
-    })
+export default function Airplane(props) {
+  const { nodes, materials } = useGLTF("./models/airplane/model.glb");
 
-    return (
-        <group {...props} dispose={null}>
-            <mesh geometry={nodes.PUSHILIN_Plane_Circle000.geometry} material={materials.plane} />
-            <mesh ref={helix} geometry={nodes.PUSHILIN_Plane_Helix.geometry} material={materials.plane} position={[1.09, 0.23, 0]} />
-        </group>
-    )
-};
+  const helix = useRef();
 
-useGLTF.preload('/models/airplane/model.glb');
-export default Airplane;
+  useFrame((_state, delta) => {
+    helix.current.rotation.x += delta * HELIX_SPEED;
+  });
+
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        geometry={nodes.PUSHILIN_Plane_Circle000.geometry}
+        material={materials.plane}
+      />
+      <mesh
+        ref={helix}
+        geometry={nodes.PUSHILIN_Plane_Helix.geometry}
+        material={materials.plane}
+        position={[1.09, 0.23, 0]}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("./models/airplane/model.glb");
